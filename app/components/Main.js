28b20e5f351cefed598104b87e91828c7b081a4e
AntonEmery <antonemery@gmail.com>
2016-06-27 22:18:07 -0700

    basic header setup

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,9 +5,12 @@ var Main = React.createClass({
 	render: function() {
 		return (
 			<div className="container">
-				<div className="row header">
-					<div className="col-md-12">
-						<p>Header</p>
+				<div className="row">
+					<div className="col-md-12 header">
+						<a id="ycLogo" href="http://www.ycombinator.com">
+							<img src="https://news.ycombinator.com/y18.gif" />
+						</a>
+						<h1 id="title">Hacker News</h1>
 					</div>
 				</div>
 					{this.props.children}
--- app/components/Posts.js
+++ app/components/Posts.js
@@ -3,7 +3,11 @@ var React = require('React');
 var Posts = React.createClass({
 	render: function() {
 		return (
-			<p>All Posts</p>
+			<div className="row">
+				<div className="col-md-12">
+					<p>All Posts</p>
+				</div>
+			</div>
 		);
 	}
 });
