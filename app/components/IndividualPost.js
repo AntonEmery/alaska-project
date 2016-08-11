26ed58a824260d445ef60d04b1a508c3e08500af
AntonEmery <antonemery@gmail.com>
2016-08-11 14:31:03 -0700

    api request

--- app/components/IndividualPost.js
+++ app/components/IndividualPost.js
@@ -1,17 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-function puke(obj) {
-		return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
+
 
 var IndividualPost =  React.createClass({
-  render: function(){
-  	console.log(this.props.post);
-  	var testData = this.props.post.map(function(item) {
-  		return <div>{item.text} {item.feed_type}</div>
-  	});
-    return <div>{testData}</div>
+	render: function(){
+		console.log(this.props.post);
+		var testData = this.props.post.map(function(item) {
+			return <div><p>{item.text}</p><p>{item.feed_type}</p></div>
+		});
+		return <div>{testData}</div>
 	}
 });
 
