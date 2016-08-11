63729e146c7e3b62378344071fe8ed6d11d9f7f1
AntonEmery <antonemery@gmail.com>
2016-08-11 15:04:17 -0700

    spaces

--- app/components/IndividualPost.js
+++ app/components/IndividualPost.js
@@ -4,14 +4,15 @@ var ReactDOM = require('react-dom');
 
 
 var IndividualPost =  React.createClass({
-	render: function(){
-		console.log(this.props.post);
-		var testData = this.props.post.map(function(item) {
-			return <p>{item}</p>
-		});
-		return <div>{testData}</div>
-	}
+  render: function(){
+    console.log(this.props.post);
+    var testData = this.props.post.map(function(item) {
+      JSON.stringify(item);
+      return <p>{item}</p>
+    });
+    return <div>{testData}</div>
+  }
 });
 
-module.exports = IndividualPost
+module.exports = IndividualPost;
 
