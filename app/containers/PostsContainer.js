bc5231eebe1c46783b780cb8c0db0cccf8d6b737
AntonEmery <antonemery@gmail.com>
2016-08-11 15:26:17 -0700

    fix spaces

--- app/containers/PostsContainer.js
+++ app/containers/PostsContainer.js
@@ -15,21 +15,18 @@ var PostsContainer = React.createClass({
 	},
 	componentDidMount: function() {
 		var that = this;
-		
-			jsonp('https://platform.postano.com/apiproxy/jsonp/accounts/4299/projects/82658/products/13196/posts', null, function (err, data) {
-  		if (err) {
-    console.error(err.message);
+		jsonp('https://platform.postano.com/apiproxy/jsonp/accounts/4299/projects/82658/products/13196/posts', null, function (err, data) {
+  	if (err) {
+    	console.error(err.message);
   } else {
    		var dataArray = [];
    		for(i=0; i<data.length; i++) {
    			dataArray.push(data[i]);
    		}
    		that.setState({loading: false, posts: dataArray});
-  }
-})
-
-
-	},
+  	}
+	})
+},
 	render: function() {
 		return (
 			<Posts   
