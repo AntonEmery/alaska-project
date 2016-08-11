a2dd26fa1c6506224e0e2716e0b1b24a4ad44492
AntonEmery <antonemery@gmail.com>
2016-08-11 15:32:33 -0700

    spaces

--- app/containers/PostsContainer.js
+++ app/containers/PostsContainer.js
@@ -6,34 +6,34 @@ var jsonp = require('jsonp');
 
 
 var PostsContainer = React.createClass({
-	getInitialState: function() {
-		return {
-			loading: true,
-			posts: []
-		};
-	},
-	componentDidMount: function() {
-		var that = this;
-		jsonp('https://platform.postano.com/apiproxy/jsonp/accounts/4299/projects/82658/products/13196/posts', null, function (err, data) {
-  	if (err) {
-    	console.error(err.message);
+  getInitialState: function() {
+    return {
+      loading: true,
+      posts: []
+    };
+  },
+  componentDidMount: function() {
+    var that = this;
+    jsonp('https://platform.postano.com/apiproxy/jsonp/accounts/4299/projects/82658/products/13196/posts', null, function (err, data) {
+    if (err) {
+      console.error(err.message);
   } else {
-   		var dataArray = [];
-   		for(i=0; i<data.length; i++) {
-   			dataArray.push(data[i]);
-   		}
-   		that.setState({loading: false, posts: dataArray});
-  	}
-	})
+      var dataArray = [];
+      for(i=0; i<data.length; i++) {
+        dataArray.push(data[i]);
+      }
+      that.setState({loading: false, posts: dataArray});
+    }
+  })
 },
   render: function() {
-		return (
-			<Posts   
-			data={this.state.posts}
-			loading={this.state.loading}
-			/>
-		);
-	}
+    return (
+      <Posts   
+      data={this.state.posts}
+      loading={this.state.loading}
+      />
+    );
+  }
 });
 
 module.exports = PostsContainer;
