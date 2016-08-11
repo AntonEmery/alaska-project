78c8f5a02a09eed3b5c4802471c9fd9a8fbe72d7
AntonEmery <antonemery@gmail.com>
2016-08-11 14:47:25 -0700

    individual posts

--- app/components/IndividualPost.js
+++ app/components/IndividualPost.js
@@ -7,7 +7,7 @@ var IndividualPost =  React.createClass({
 	render: function(){
 		console.log(this.props.post);
 		var testData = this.props.post.map(function(item) {
-			return <div><p>{item.text}</p><p>{item.feed_type}</p></div>
+			return <p>{item}</p>
 		});
 		return <div>{testData}</div>
 	}
