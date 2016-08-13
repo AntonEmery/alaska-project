9e8a3b314a2eb96ad7e3bd94517bbafeadacf29d
AntonEmery <antonemery@gmail.com>
2016-08-13 12:12:38 -0700

    need to render api data on details page

--- app/components/PostDetails.js
+++ app/components/PostDetails.js
@@ -2,8 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var PostDetails = React.createClass({
+
+  getInitialState: function() {
+    return {
+      item: parseInt(this.props.params.index)
+    }
+  },
   render: function(){
-  console.log(this.props);
+  console.log(this.props)
     return (
       <div>
       <p>{this.props.params.index}</p>
