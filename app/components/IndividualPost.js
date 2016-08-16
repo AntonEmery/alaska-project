8e60da736e77e87dcbb63c2a857ab5dfb6b1c7a4
AntonEmery <antonemery@gmail.com>
2016-08-15 20:45:45 -0700

    rendering image

--- app/components/IndividualPost.js
+++ app/components/IndividualPost.js
@@ -4,15 +4,28 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Link = ReactRouter.Link;
 
-
-
 var IndividualPost =  React.createClass({
+
+  renderImage: function(arrayItem) {
+    if (!arrayItem.images.length) {
+      return (
+        <p>No image</p>
+      )
+    } else {
+        return (
+          <img src={arrayItem.images[0].url} />
+        )
+    }
+  },
   render: function(){
+    var that = this;
     var testData = this.props.post.map(function(item, index) {
+      console.log(item);
       return (
         <div> 
           <p key={index}>{item.feed_type}</p>
           <Link to={"PostDetails/" + index}>Details</Link>
+          {that.renderImage(item)} 
         </div>
 
       )
